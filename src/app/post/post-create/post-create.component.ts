d9e1354acc1d2b7fd2de4917022652def5fc7085
Dave Flores Malhin <malhindave@gmail.com>
2021-07-22 02:08:48 +0800

    Just update

--- src/app/post/post-create/post-create.component.ts
+++ src/app/post/post-create/post-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-create',
@@ -9,13 +11,37 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class PostCreateComponent {
   entererdTitle = "";
   entererdContent = "";
-  @Output() postCreated = new EventEmitter();
+  @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
-    const post = {
-      title: this.entererdTitle,
-      content: this.entererdContent
-    };
-    this.postCreated.emit(post);
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    } else {
+      const post: Post = {
+        title: form.value.title,
+        content: form.value.content
+      };
+      this.postCreated.emit(post);
+    }
+  }
+  getTitleMessage(){
+    return 'Please Enter Title.';
+  }
+  getContentMessage(){
+    return 'Please Enter Content.';
   }
 }
+
+// export class PostCreateComponent {
+//   entererdTitle = "";
+//   entererdContent = "";
+//   @Output() postCreated = new EventEmitter<Post>();
+
+//   onAddPost() {
+//     const post: Post = {
+//       title: this.entererdTitle,
+//       content: this.entererdContent
+//     };
+//     this.postCreated.emit(post);
+//   }
+// }
